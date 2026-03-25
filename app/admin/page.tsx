0647369e1e76ba30a7254d8271b15e2829f3ef44
Ahmed <ahmedkiller0234@gmail.com>
2026-03-26 00:28:16 +0500

    A lot of authentication changes, and implemented clerk Authentication and database work

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Admin Page you can not access it !</h1>
+    </div>
+  );
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/ui/theme-toggler';
+import { SignInButton, UserButton } from '@clerk/nextjs';
 
-const page = () => {
+const page = async () => {
   return (
     <div>
+      <UserButton />
       <h1>Hello world !</h1>
       <Button className="cursor-pointer">Click me !</Button>
       <ModeToggle />
+      <SignInButton mode="modal">
+        <Button>Checkout</Button>
+      </SignInButton>
     </div>
   );
 };
